7d630e61b06091deb057abb7596fbd2a0d7ed0a4
kaflepukar <pukar@smartcontents.com.np>
2025-10-09 10:54:15 +0545

    setup logging and middleware (#2)

--- main.py
+++ main.py
@@ -1,8 +1,33 @@
-from fastapi import FastAPI
+from typing import Awaitable, Callable
+from uuid import uuid4
 
-app = FastAPI()
+from fastapi import FastAPI, Request, Response
+
+from settings import settings
+from utils.logger import get_logger, request_id_ctx_var
+
+logger = get_logger()
+
+app = FastAPI(title="Bizchat-AI")
 
 
-@app.get("/health", tags=["Health"])
-async def health():
-    return "okey"
+@app.middleware("http")
+async def logging_middleware(
+    request: Request, call_next: Callable[[Request], Awaitable[Response]]
+) -> Response:
+    request_id = str(uuid4())
+    request_id_ctx_var.set(request_id)
+    message = f"{request.method} {request.url.path}"
+    extra = {}
+    if settings.ENV == "local":
+        extra["query"] = request.query_params  # type: ignore
+    logger.info(message, extra=extra)
+    response = await call_next(request)
+    response.headers["X-Request-ID"] = request_id
+    return response
+
+
+@app.get("/healthz", tags=["Health"])
+async def healthz():
+    logger.info("Health check")
+    return "ok!"
